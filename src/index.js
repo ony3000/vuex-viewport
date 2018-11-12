fbbbb4426f583aae1143dc59dc82ff4a8f22cccc
Hyeonjong <nianelo4@gmail.com>
2018-11-12 10:24:10 +0900

    Add base structure of module

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const viewport = {
+    namespaced: true,
+    state() {
+        return {
+            width: 0,
+            height: 0,
+        };
+    },
+    getters: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+};
+
+export {
+    viewport,
+};
