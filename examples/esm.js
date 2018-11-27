be7f11168ff8eefc2116f5f748d3bfca32fac406
Hyeonjong <nianelo4@gmail.com>
2018-11-28 01:16:12 +0900

    Update use-case; ES2015 module import

--- examples/esm.js
+++ examples/esm.js
@@ -1,15 +1,25 @@
 import Vue from 'vue/dist/vue.esm';
-import Vuex, { mapState } from 'vuex';
+import Vuex, { mapState, mapGetters } from 'vuex';
 import { storeModule, createPlugin } from 'vuex-viewport';
 
 Vue.use(Vuex);
+
+const pluginOptions = {
+    delay: 200,
+    maxDelay: 1000,
+    breakpoints: {
+        tablet: 768,
+        desktop: 992,
+        largeDesktop: 1200
+    }
+};
 
 const store = new Vuex.Store({
     modules: {
         viewport: storeModule
     },
     plugins: [
-        createPlugin()
+        createPlugin(pluginOptions)
     ]
 });
 
@@ -17,6 +27,9 @@ new Vue({
     el: '#app',
     store,
     computed: {
+        ...mapGetters({
+            layoutType: 'viewport/mediaName'
+        }),
         ...mapState({
             windowWidth: state => state.viewport.width,
             windowHeight: state => state.viewport.height
