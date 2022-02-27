d8e112689b7816cbb66fe5bb0e9a55feb525bef0
Hyeonjong <nianelo4@gmail.com>
2022-02-27 20:49:15 +0900

    Update webpack config to make available on both browser and Node.js
    Reference: https://v4.webpack.js.org/configuration/output/#outputglobalobject

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'vuex-viewport.js',
+    globalObject: 'this',
     library: 'vuexViewport',
     libraryTarget: 'umd'
   },
